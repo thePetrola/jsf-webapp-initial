477b9b472670812fd5f1670c0cabc432e3dd0c5b
lucas.petrola <lucas.petrola@saude.gov.br>
2024-08-11 21:42:37 -0300

    Initial commit

---
+++ src/main/java/br/com/opetrola/utils/EntityManagerProducer.java
@@ -0,0 +1,29 @@
+package br.com.opetrola.utils;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+@ApplicationScoped
+public class EntityManagerProducer {
+
+    EntityManagerFactory factory;
+
+    public EntityManagerProducer() {
+        this.factory = Persistence.createEntityManagerFactory("baseUnitPU");
+    }
+
+    @Produces
+    @RequestScoped
+    public EntityManager getEntityManager() {
+        return factory.createEntityManager();
+    }
+
+    public void close(@Disposes EntityManager em) {
+        em.close();
+    }
+}
